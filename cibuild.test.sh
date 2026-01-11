23d83f03c5838a9c5d7a492fbf329b2215b20524
Stefan Schneider <eqsoft4@gmail.com>
2026-01-11 10:10:48 +0100

    initial full branch

--- cibuild.test.sh
+++ cibuild.test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-assert_log "testlog" "/bin/sh" "-c" "echo 'testlog'; sleep infinity"
-#assert_response 80 "Welcome to nginx"
+#assert_log "testlog" "/bin/sh" "-c" "echo 'testlog'; sleep infinity"
+assert_response 80 "Welcome to nginx"
